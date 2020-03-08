54403a7848ab8714ef3772da0c121c91ece163ab
y293651123 <y293651123@gmail.com>
2020-03-08 21:59:53 +0800

    First commit

---
+++ mbed-os-test-program3/main.cpp
@@ -0,0 +1,29 @@
+#include "mbed.h"
+
+
+DigitalOut redLED(LED_RED);
+
+DigitalOut greenLED(LED_GREEN);
+
+
+int Led(DigitalOut& ledpin);
+int Led1(DigitalOut& ledpin);
+
+
+int main()
+
+{
+
+   redLED = 1;
+
+   greenLED = 1;
+
+   while (true) {
+
+       Led(redLED);
+
+       Led1(greenLED);
+
+   }
+
+}
